0942ecaa2a7853e5afb03b66c807148cde08b042
VicMontanez <v_montanez@hotmail.com>
2019-08-26 17:46:07 -0400

    printf

---
+++ printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%d", 500); //prints out integer
+    printf("%s");      //prints out string of text
+    printf("%f", 3.7); //prints out float(decimal)
+
+    return 0;
+}
