5e0b2f7f08df5d01fe5dacb36e0eb8aed28c164c
VicMontanez <v_montanez@hotmail.com>
2019-09-09 00:47:52 -0400

    guessing

--- guessing.c
+++ guessing.c
@@ -3,7 +3,32 @@
 
 int main()
 {
-    
+    int secretNumber = 5;
+    int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    int outOfGuesses = 0;
 
+    while (guess != secretNumber && outOfGuesses == 0)
+    {
+        if (guessCount < guessLimit)
+        {
+            printf("Enter a number: ");
+            scanf("%d", &guess);
+            guessCount++;
+        }
+        else
+        {
+            outOfGuesses = 1;
+        }
+    }
+    if (outOfGuesses == 1)
+    {
+        printf("Out of guesses");
+    }
+    else
+    {
+        printf("You win!\n");
+    }
     return 0;
 }
