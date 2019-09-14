2377ac9efff11fc13510534f9f59bbaa9db50cd0
VicMontanez <v_montanez@hotmail.com>
2019-09-14 19:44:32 -0400

    nestedArrays

---
+++ nestedArrays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nums[3][2] =
+        {
+            {1, 2}, {3, 4}, {5, 6}};
+    int i, j; //you can decalre two variables in one line
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d,", nums[i][j]);
+        }
+        printf("\n");
+    }
+}
